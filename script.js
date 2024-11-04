68c4b351312113eea93d9f12798a30d86e204174
amasson <antoine.masson@withings.com>
2024-11-04 16:47:08 +0100

    added texts

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.getElementById("spoiler").addEventListener("click", function() {
-    var imageContainer = document.getElementById("image-container");
-    if (imageContainer.style.display === "none") {
-        imageContainer.style.display = "block";
-    } else {
-        imageContainer.style.display = "none";
-    }
+  var imageContainer = document.getElementById("image-container");
+  if (imageContainer.style.display === "none") {
+    imageContainer.style.display = "block";
+    setTimeout(() => {
+      const textDiv = document.createElement('div');
+      textDiv.textContent = '(Je vais être papa)';
+      textDiv.style.textAlign = 'center';
+      textDiv.style.marginTop = '10px';
+      imageContainer.appendChild(textDiv);
+    }, 2000); // wait for 2 seconds before adding the text
+  } else {
+    imageContainer.style.display = "none";
+  }
 });
